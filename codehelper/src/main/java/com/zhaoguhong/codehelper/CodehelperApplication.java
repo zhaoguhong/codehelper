a384588653264b165eeaf4e7cf83d5667b5c8fc5
zhaoguhong <761569559@qq.com>
2019-01-12 23:41:01 +0800

    init

---
+++ codehelper/src/main/java/com/zhaoguhong/codehelper/CodehelperApplication.java
@@ -0,0 +1,14 @@
+package com.zhaoguhong.codehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodehelperApplication.class, args);
+	}
+
+}
+
